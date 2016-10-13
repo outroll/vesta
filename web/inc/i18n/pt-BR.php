cf76debf93962a5c87323f71cc42f8fbfbd6e163
dpeca <peca@mycity.rs>
2016-10-14 01:56:35 +0200

    Update pt-BR.php

--- web/inc/i18n/pt-BR.php
+++ web/inc/i18n/pt-BR.php
@@ -746,5 +746,6 @@ $LANG['pt-BR'] = array(
     'No encryption' => 'Nenhuma criptografia',
     'Do not use encryption' => 'Não usar criptografia',
 
-    'maximum characters length, including prefix' => 'comprimento máximo de % caracteres, incluindo o prefixo',
+    'maximum characters length, including prefix' => 'maximum %s characters length, including prefix',
+
 );
