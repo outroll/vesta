6f3041251adfa3321132a2c7499c9bea531c16fe
Marcos <djoser.horus@gmail.com>
2016-12-25 16:36:57 +0100

    Update caching.sh
    If you activate and deactivate Nginx caching template, next time it's activated will fail because of duplicate entries in /etc/nginx/conf.d/01_caching_pool.conf
    With this fix, it will check and delete duplicate entries before restarting Nginx, thus enabling caching correctly for the domain and any others which were active before.

--- install/rhel/7/templates/web/nginx/caching.sh
+++ install/rhel/7/templates/web/nginx/caching.sh
@@ -9,4 +9,4 @@ docroot=$5
 str="proxy_cache_path /var/cache/nginx/$domain levels=2"
 str="$str keys_zone=$domain:10m inactive=60m max_size=512m;"
 echo "$str" >> /etc/nginx/conf.d/01_caching_pool.conf
-
+sort -u /etc/nginx/conf.d/01_caching_pool.conf -o /etc/nginx/conf.d/01_caching_pool.conf
