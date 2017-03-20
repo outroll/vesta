30f7c51eef85be60a8fb0251539a2442de6e16a7
info@madeit.be <info@madeit.be>
2017-03-20 08:48:17 +0000

    Dockerfile
    Dockerfile
    
    Dockerfile
    
    Dockerfile
    
    Dockerfile
    
    Dockerfile
    
    Dockerfile
    
    Dockerfile
    
    Dockerfile
    
    Dockerfile
    
    Dockerfile
    
    Dockerfile
    
    Fix tests
    
    Fix tests + docker permissioné
    
    Fix tests
    
    Fix tests
    
    Fix tests
    
    Fix tests
    
    Fix tests
    
    Fix tests
    
    Fix tests
    
    Fix tests
    
    Integrate CI and docker; Fix tests and code

--- docker/startup.sh
+++ docker/startup.sh
@@ -2,10 +2,14 @@
 
 export TERM=xterm
 
+PATH=$PATH:/usr/local/vesta/bin
+export PATH
+
 #starting Vesta
 cd /etc/init.d/ \
 && ./vesta start \
 && ./mysql start \
+&& ./bind9 start \
 && ./nginx start \
 && ./exim4 start \
 && ./apache2 start \
