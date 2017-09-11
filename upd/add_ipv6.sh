482e19b06f8d2fe15c5754fafeb4b34327fbcf15
Made I.T <info@madeit.be>
2017-09-11 23:33:41 +0200

    Update add_ipv6.sh

--- upd/add_ipv6.sh
+++ upd/add_ipv6.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 source /etc/profile.d/vesta.sh
-
-#download new templates
-$BIN/v-update-web-templates
-$BIN/v-update-dns-templates
+source /usr/local/vesta/func/main.sh
 
 #Download firewallv6 templates
 if [ ! -e "$VESTA/data/firewallv6" ]; then
@@ -16,15 +13,21 @@ if [ ! -e "$VESTA/data/firewallv6" ]; then
 
 fi
 
+#set IPv4 version
+iplist=$(ls --sort=time $VESTA/data/ips/)
+for ip in $iplist; do
+	echo "VERSION='4'" >> $VESTA/data/ips/$ip
+done
+
 #Add IP6 field
 ipv6=$(ip addr show | sed -e's/^.*inet6 \([^ ]*\)\/.*$/\1/;t;d' | grep -ve "^fe80" | tail -1)
 ipv6use=""
-if [ ! -z "$ipv6" ]; then
-    netmask="ip addr show | grep '$ipv6' | awk -F '/' '{print $2}' | awk '{print $1}'"
+if [ ! -z "$ipv6" ] && [ "::1" != "$ipv6" ]; then
+    netmask="ip addr show | grep '$ipv6' | awk -F '/' '{print \$2}' | awk '{print \$1}'"
     netmask=$(eval $netmask)
-    $BIN/v-add-sys-ipv6 $ipv6 $netmask
+    $VESTA/bin/v-add-sys-ipv6 $ipv6 $netmask
     $BIN/v-update-firewall-ipv6
-    ipv6use=ipv6
+    ipv6use=$ipv6
 fi
 
 #set IPv6
@@ -51,5 +54,10 @@ for user in $userlist; do
     done < $conf
     $BIN/v-rebuild-user $user
 done
+
+
+#download new templates
+$BIN/v-update-web-templates
+$BIN/v-update-dns-templates
 
 /usr/local/vesta/bin/v-add-user-notification admin "IPv6 support" "Your vesta installation supports IPv6!"
