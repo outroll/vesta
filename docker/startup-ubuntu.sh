e7f5b60a831c15862869f0fdcc4b328f3443af3e
info@madeit.be <info@madeit.be>
2017-09-23 15:51:08 +0000

    Try to fix mysql in docker

--- docker/startup-ubuntu.sh
+++ docker/startup-ubuntu.sh
@@ -6,11 +6,14 @@ PATH=$PATH:/usr/local/vesta/bin
 export PATH
 
 #starting Vesta
-cd /etc/init.d/ \
-&& ./vesta start \
-&& ./mysql start \
-&& ./bind9 start \
-&& ./nginx start \
-&& ./exim4 start \
-&& ./apache2 start \
-&& ./dovecot start
+cd /etc/init.d/
+./vesta start
+./bind9 start
+./nginx start
+./exim4 start
+./apache2 start
+./dovecot start
+./mysql start
+if [ "$?" -gt 0 ]; then
+    ./mysql start
+fi
