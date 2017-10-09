bee1bb37f408fec5093b433c2167d22d4b3e8df3
Made I.T <info@madeit.be>
2017-10-09 20:02:31 +0000

    Monitoring

--- upd/add_plugin.sh
+++ upd/add_plugin.sh
@@ -4,5 +4,5 @@ source /usr/local/vesta/func/main.sh
 
 touch $VESTA/conf/plugin.conf
 echo "PLUGIN='hello-world' NAME='Hello World' VERSION='1.0.0' BUILDNUMBER='1' LATEST_VERSION='1.0.0' LATEST_BUILDNUMBER='1' KEY='' ACTIVE='yes' TIME='22:28:00' DATE='2017-09-23'" >> $VESTA/conf/plugin.conf 
-echo "PLUGIN='monitor' NAME='Monitor' VERSION='1.0.0' BUILDNUMBER='1' LATEST_VERSION='1.0.0' LATEST_BUILDNUMBER='1' KEY='' ACTIVE='yes' TIME='21:59:00' DATE='2017-10-09'" >> $VESTA/conf/plugin.conf 
-echo "PLUGIN='monitor-dashboard' NAME='Monitor Dashboard' VERSION='1.0.0' BUILDNUMBER='1' LATEST_VERSION='1.0.0' LATEST_BUILDNUMBER='1' KEY='' ACTIVE='yes' TIME='21:59:00' DATE='2017-10-09'" >> $VESTA/conf/plugin.conf 
+echo "PLUGIN='monitor' NAME='Monitor' VERSION='1.0.0' BUILDNUMBER='1' LATEST_VERSION='1.0.0' LATEST_BUILDNUMBER='1' KEY='' ACTIVE='no' TIME='21:59:00' DATE='2017-10-09'" >> $VESTA/conf/plugin.conf 
+echo "PLUGIN='monitor-dashboard' NAME='Monitor Dashboard' VERSION='1.0.0' BUILDNUMBER='1' LATEST_VERSION='1.0.0' LATEST_BUILDNUMBER='1' KEY='' ACTIVE='no' TIME='21:59:00' DATE='2017-10-09'" >> $VESTA/conf/plugin.conf 
