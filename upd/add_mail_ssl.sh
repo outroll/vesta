bdb4d451432ca391e8b71f4e59758343681af00c
Made I.T <info@madeit.be>
2017-10-27 14:48:20 +0000

    Custom e-mail ssl

--- upd/add_mail_ssl.sh
+++ upd/add_mail_ssl.sh
@@ -4,6 +4,20 @@
 
 #Add U_MAIL_SSL='0' to all users
 
+userlist=$(ls --sort=time $VESTA/data/users/)
+for user in $userlist; do
+    USER_DATA="$VESTA/data/users/$user"
 
-add_object_key "mail" 'DOMAIN' "$domain_idn" 'LETSENCRYPT' 'SUSPENDED'
-update_object_value 'mail' 'DOMAIN' "$domain_idn" '$LETSENCRYPT' 'no'
+    #UPDATE mail
+    conf="$USER_DATA/mail.conf"
+    while read line ; do
+        eval $line
+        
+        add_object_key "mail" 'DOMAIN' "$DOMAIN" 'SSL' 'SUSPENDED'
+        update_object_value 'mail' 'DOMAIN' "$DOMAIN" '$SSL' 'no'
+        
+        add_object_key "mail" 'DOMAIN' "$DOMAIN" 'LETSENCRYPT' 'SUSPENDED'
+        update_object_value 'mail' 'DOMAIN' "$DOMAIN" '$LETSENCRYPT' 'no'
+    done < $conf
+
+done
