9b24931102de8da778c717bfe304e346178ebec9
UrbanZan <urban.zan@gmail.com>
2018-01-12 08:54:13 +0100

    add dovecot-sieve dovecot-managesieved

--- install/vst-install-ubuntu.sh
+++ install/vst-install-ubuntu.sh
@@ -43,7 +43,7 @@ elif [ "$release" = '16.10' ]; then
         flex whois rssh git idn zip sudo bc ftp lsof ntpdate rrdtool quota
         e2fslibs bsdutils e2fsprogs curl imagemagick fail2ban dnsutils
         bsdmainutils cron vesta vesta-nginx vesta-php expect vim-common
-        vesta-ioncube vesta-softaculous"
+        vesta-ioncube vesta-softaculous dovecot-sieve dovecot-managesieved"
 else
     software="nginx apache2 apache2-utils apache2.2-common
         apache2-suexec-custom libapache2-mod-ruid2 libapache2-mod-rpaf
