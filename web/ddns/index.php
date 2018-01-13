a04b7fd5d44a3269d131d8f24082b0d2eef75f9a
Ryan Brownell <ryan.brownell@gmail.com>
2018-01-13 19:10:46 -0400

    stubbed out ddns page.

---
+++ web/ddns/index.php
@@ -0,0 +1,6 @@
+<?php
+
+// TODO: accept both POST and GET methods.
+// TODO: do not accept non SSL requests.
+// TODO: Create method to authenticate the DDNS key and update DNS records here.
+//system(), exec(), passthru() and backticks. None of them protects you from shell injection by default. You have to use escapeshellarg() or escapeshellcmd().
