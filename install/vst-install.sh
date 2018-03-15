360344211d57c8792da64e8d124e7b2272f28989
Luís Pedro Algarvio <lp.algarvio@gmail.com>
2018-03-15 16:35:30 +0000

    Reimplemented os detection
    Rely on /etc/os-release, lsb_release, /etc/lsb-release, /etc/system-release and /etc/redhat-release to get the proper distribution information for the supported distributions (debian, ubuntu, centos, rhel)

--- install/vst-install.sh
+++ install/vst-install.sh
@@ -37,17 +37,105 @@ if [ ! -z "$(grep ^admin: /etc/group)" ] && [ -z "$1" ]; then
     exit 1
 fi
 
-# Detect OS
-case $(head -n1 /etc/issue | cut -f 1 -d ' ') in
-    Debian)     type="debian" ;;
-    Ubuntu)     type="ubuntu" ;;
-    *)          type="rhel" ;;
-esac
 
-# Fallback to Ubuntu
-if [ ! -e "/etc/redhat-release" ]; then
-    type='ubuntu'
-fi
+#----------------------------------------------------------#
+#                 Check system information                 #
+#----------------------------------------------------------#
+
+# Lookup /etc/os-release file on systemd and newer distros
+if [ -r "/etc/os-release" ]; then
+  source  /etc/os-release;
+  os=$ID;
+  release=$(echo $VERSION_ID | cut -d . -f 1);
+  # Get codename from variable or from other sources
+  if [ -n "$VERSION_CODENAME" ]; then
+    codename=$VERSION_CODENAME;
+  else
+    case $os in
+      debian)
+        codename=$(awk -F"[)(]+" '/VERSION=/ {print $2}' /etc/os-release);
+        ;;
+      ubuntu)
+        codename=$(echo "${VERSION/*, /}" | awk '{print $1}' | tr '[:upper:]' '[:lower:]');
+        ;;
+      centos|rhel)
+        codename="${os}$release";
+        ;;
+    esac;
+  fi;
+
+# Lookup lsb_release binary on newer distros
+elif [ type lsb_release >/dev/null 2>&1 ]; then
+  os=$(lsb_release -si);
+  release=$(lsb_release -sr | cut -d . -f 1);
+  codename=$(lsb_release -sc);
+
+# Lookup /etc/lsb-release file on Ubuntu based distros
+elif [ -r "/etc/lsb-release" ]; then
+  source  /etc/lsb-release;
+  os=$($DISTRIB_ID | tr '[:upper:]' '[:lower:]');
+  release=$(echo $DISTRIB_RELEASE | cut -d . -f 1);
+  codename=$($DISTRIB_CODENAME | tr '[:upper:]' '[:lower:]');
+
+# Lookup /etc/system-release file on newer Red Hat based distros
+elif [ -r "/etc/system-release" ]; then
+  os=$(cut -f 1 -d ' ' /etc/system-release | tr '[:upper:]' '[:lower:]');
+  release=$(grep -o "[0-9]" /etc/system-release | head -n 1);
+  codename="${os}$release";
+
+# Lookup /etc/redhat-release file on older Red Hat based distros
+elif [ -r "/etc/redhat-release" ]; then
+  os=$(cut -f 1 -d ' ' /etc/redhat-release | tr '[:upper:]' '[:lower:]');
+  release=$(grep -o "[0-9]" /etc/redhat-release | head -n 1);
+  codename="${os}$release";
+
+# Unknown system
+else
+  echo "Error: could not detect system or distribution";
+  exit 1;
+fi;
+
+# Validate distribution, release codename and set type
+printf "\nDetected distribution ${os} ${release} (${codename})\n";
+case $os in
+    debian)
+      type="debian";
+      supported_releases=("7 8 9");
+      supported_codenames=("wheezy jessie stretch");
+      ;;
+
+    ubuntu)
+      type="ubuntu";
+      supported_releases=("12 14 16");
+      supported_codenames=("precise trusty xenial");
+      ;;
+
+    centos)
+      type="centos";
+      supported_releases=("5 6 7");
+      supported_codenames=("centos5 centos6 centos7");
+      ;;
+
+    centos)
+      type="rhel";
+      supported_releases=("5 6 7");
+      supported_codenames=("rhel5 rhel6 rhel7");
+      ;;
+
+    *)
+      printf "\nUnsupported distribution\n";
+      exit 1;
+      ;;
+esac;
+if [[ ! " ${supported_releases[@]} " =~ " ${release} " ]]; then
+  printf "\nUnsupported distribution release\n";
+  exit 1;
+fi;
+if [[ ! " ${supported_codenames[@]} " =~ " ${codename} " ]]; then
+  printf "\nUnsupported distribution codename\n";
+  exit 1;
+fi;
+
 
 # Check wget
 if [ -e '/usr/bin/wget' ]; then
