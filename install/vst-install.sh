f6edb18850339e226d7d84ae28eea10088ef3880
Luís Pedro Algarvio <lp.algarvio@gmail.com>
2018-03-15 16:42:35 +0000

    Added section to install minimum requirements
    Install some global requirements, such as package management system (apt/yum) packages, openssl, ca-certificates, gnupg, wget/curl, dialog, which, etc) and configure the package management system, with special provisions for the obsolete CentOS 5

--- install/vst-install.sh
+++ install/vst-install.sh
@@ -137,6 +137,89 @@ if [[ ! " ${supported_codenames[@]} " =~ " ${codename} " ]]; then
 fi;
 
 
+#----------------------------------------------------------#
+#               Install minimum requirements               #
+#----------------------------------------------------------#
+
+# Debian and Ubuntu \
+if [ "$os" == "debian" ] || [ "$os" == "ubuntu" ]; then \
+  printf "Installing minimum requirements...\n" && \
+  \
+  printf "Disable installation of optional apt packages...\n" && \
+  printf "\n# Disable recommended and suggested packages\n\
+APT::Install-Recommends "\""false"\"";\n\
+APT::Install-Suggests "\""false"\"";\n\
+\n" >> /etc/apt/apt.conf && \
+  \
+  # Fix Debian repository configuration \
+  if [ "$os" == "debian" ]; then \
+    mkdir -p /var/cache/yum/{base,extras,updates,centosplus};
+  printf "Enable contrib and non-free components in debian repositories...\n" && \
+  sed -i "s>main>main contrib non-free>" /etc/apt/sources.list; \
+  fi && \
+  \
+  printf "Refresh the package manager...\n" && \
+  apt-get update && \
+  \
+  printf "Install the package manager packages...\n" && \
+  apt-get install -qy \
+    apt-utils apt-transport-https && \
+  \
+  printf "Install the selected packages...\n" && \
+  apt-get install -qy \
+    dialog debianutils sed grep wget curl \
+    gnupg gpgv dirmngr openssl ca-certificates && \
+  \
+  printf "Finished installing minimum requirements...\n"; \
+  \
+# CentOS and RHEL \
+elif [ "$os" == "centos" ] || [ "$os" == "rhel" ]; then \
+  # Fix CentOS 5 repository configuration \
+  if [ "$os" == "centos" ] && [ "$release" == "5" ]; then \
+    mkdir -p /var/cache/yum/{base,extras,updates,centosplus} && \
+    echo "http://vault.centos.org/5.11/os/x86_64/" > /var/cache/yum/base/mirrorlist.txt && \
+    echo "http://vault.centos.org/5.11/extras/x86_64/" > /var/cache/yum/extras/mirrorlist.txt && \
+    echo "http://vault.centos.org/5.11/updates/x86_64/" > /var/cache/yum/updates/mirrorlist.txt && \
+    echo "http://vault.centos.org/5.11/centosplus/86_64/" > /var/cache/yum/centosplus/mirrorlist.txt && \
+    sed -i '/\libselinux\]/,/^ *\[/ s/enabled=1/enabled=0/' /etc/yum.repos.d/libselinux.repo; \
+  fi && \
+  \
+  printf "Installing minimum requirements...\n" && \
+  \
+  printf "Refresh the package manager...\n" && \
+  rpm --rebuilddb && yum makecache && \
+  \
+  printf "Install the package manager packages...\n" && \
+  if [ "$os" == "centos" ] && [ "$release" != "5" ]; then \
+    yum install -y \
+      yum-plugin-fastestmirror yum-plugin-priorities \
+      yum-plugin-keys yum-utils; \
+  elif [ "$os" == "centos" ] && [ "$release" == "5" ]; then \
+    yum install -y \
+      yum-utils; \
+  fi && \
+  \
+  printf "Install the selected packages...\n" && \
+  if [ "$os" == "centos" ] && [ "$release" != "5" ]; then \
+    yum install -y \
+      dialog which sed grep wget curl \
+      gnupg openssl ca-certificates; \
+  elif [ "$os" == "centos" ] && [ "$release" == "5" ]; then \
+    yum downgrade -y libselinux && \
+    yum install -y \
+      dialog which sed grep wget curl \
+      gnupg openssl; \
+  fi && \
+  \
+  # Fix CentOS 5 certificates \
+  #if [ "$os" == "centos" ] && [ "$release" == "5" ]; then \
+  #  wget --no-check-certificate --secure-protocol=TLSv1 http://curl.haxx.se/ca/cacert.pem -O /etc/pki/tls/certs/ca-bundle.crt; \
+  #fi && \
+  \
+  printf "Finished installing minimum requirements...\n"; \
+fi;
+
+
 # Check wget
 if [ -e '/usr/bin/wget' ]; then
     wget http://vestacp.com/pub/vst-install-$type.sh -O vst-install-$type.sh
