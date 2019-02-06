2d96e3631f9c5e48c8aa297328060942f7707220
devius <david.machakhelidze@gmail.com>
2019-02-06 17:23:58 +0400

    Add missing translations
    I have found and translated last missing Georgian versions for strings

--- web/inc/i18n/ka.php
+++ web/inc/i18n/ka.php
@@ -744,14 +744,14 @@ $LANG['ka'] = array(
     'PUB_KEY' => 'PUB_KEY',
     'ISSUER' => 'ISSUER',
 
-    'Use server hostname' => 'Use server hostname',
-    'Use domain hostname' => 'Use domain hostname',
-    'Use STARTTLS' => 'Use STARTTLS',
-    'Use SSL / TLS' => 'Use SSL / TLS',
-    'No encryption' => 'No encryption',
-    'Do not use encryption' => 'Do not use encryption',
+    'Use server hostname' => 'სერვერის hostname-ის გამოყენება',
+    'Use domain hostname' => 'დეომენის hostname-ის გამოყენება',
+    'Use STARTTLS' => 'STARTTLS-ის გამოყენება',
+    'Use SSL / TLS' => 'SSL / TLS-ის გამოყენება',
+    'No encryption' => 'დაშიფრვის გარეშე',
+    'Do not use encryption' => 'არ გამოიყენო დაშიფრვა',
 
-    'maximum characters length, including prefix' => 'maximum %s characters length, including prefix',
+    'maximum characters length, including prefix' => 'დასაშვებია მაქსიმუმ %s სიმბოლო, პრეფიქსის ჩათვლით',
 
-    'Email Credentials' => 'Email Credentials',
+    'Email Credentials' => 'რეკვიზიტების ელ-ფოსტით გაგზავნა',
 );
