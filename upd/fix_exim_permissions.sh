493c54fc2976e753863ef90cb63553b9f734390d
pablobae <33727260+pablobae@users.noreply.github.com>
2019-03-13 17:47:52 +0100

    Update fix_exim_permissions.sh
    Fixing set dovecot user as owner of files when dovecot is not installed

--- upd/fix_exim_permissions.sh
+++ upd/fix_exim_permissions.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 
+# Define mail user
+if [ "$MAIL_SYSTEM" = 'exim4' ]; then
+    MAIL_USER=Debian-exim
+else
+    MAIL_USER=exim
+fi
+
 if [ -e "/etc/exim4/domains/" ]; then
     for domain in $(ls /etc/exim4/domains/); do
         domain_link=$(readlink /etc/exim4/domains/$domain)
-        chown Debian-exim:mail $domain_link
-        chown Debian-exim:mail /etc/exim4/domains/$domain/*
-        chown dovecot:mail /etc/exim4/domains/$domain/passwd
+        chown $MAIL_USER:mail $domain_link
+        chown $MAIL_USER:mail /etc/exim4/domains/$domain/*
+    
+        # Checking if user dovecot exists. If dovecor user doesn't exist (dovecot is not installed), owner is exim user.
+        if id "dovecot" >/dev/null 2>&1; then
+            chown -R dovecot:mail /etc/exim4/domains/$domain/passwd
+        else
+            chown -R $MAIL_USER:mail /etc/exim4/domains/$domain/passwd
+        fi
     done
 fi
 
