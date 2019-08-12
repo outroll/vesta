0423d9a5e47f04d530ad2b18c7d20df54ced1267
myvesta <38690722+myvesta@users.noreply.github.com>
2019-08-12 22:31:20 +0200

    Update secure_login.php

--- web/inc/secure_login.php
+++ web/inc/secure_login.php
@@ -21,7 +21,10 @@ if ($login_url_skip==0) {
         if (file_exists('/usr/local/vesta/web/inc/login_url.php')) {
             require_once('/usr/local/vesta/web/inc/login_url.php');
             if (isset($_GET[$login_url])) {
-                setcookie($login_url, '1', time() + 31536000, '/', $_SERVER['HTTP_HOST'], true);
+                $Domain=$_SERVER['HTTP_HOST'];
+                $Port = strpos($Domain, ':');
+                if ($Port !== false)  $Domain = substr($Domain, 0, $Port);
+                setcookie($login_url, '1', time() + 31536000, '/', $Domain, true);
                 header ("Location: /login/");
                 exit;
             }
