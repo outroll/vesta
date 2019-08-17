0634c42b2607ef2099a938faacb74a6b514e3553
dpeca <peca@mycity.rs>
2019-08-17 19:50:01 +0200

    Update vst-install-debian.sh

--- install/vst-install-debian.sh
+++ install/vst-install-debian.sh
@@ -44,6 +44,20 @@ elif [ "$release" -eq 9 ]; then
         bsdmainutils cron vesta vesta-nginx vesta-php expect libmail-dkim-perl
         unrar-free vim-common net-tools unzip"
 fi
+elif [ "$release" -eq 8 ]; then
+    software="nginx apache2 apache2-utils apache2.2-common
+        apache2-suexec-custom libapache2-mod-ruid2
+        libapache2-mod-fcgid libapache2-mod-php5 php5 php5-common php5-cgi
+        php5-mysql php5-curl php5-fpm php5-pgsql awstats webalizer vsftpd
+        proftpd-basic bind9 exim4 exim4-daemon-heavy clamav-daemon
+        spamassassin dovecot-imapd dovecot-pop3d roundcube-core
+        roundcube-mysql roundcube-plugins mysql-server mysql-common
+        mysql-client postgresql postgresql-contrib phppgadmin phpMyAdmin mc
+        flex whois rssh git idn zip sudo bc ftp lsof ntpdate rrdtool quota
+        e2fslibs bsdutils e2fsprogs curl imagemagick fail2ban dnsutils
+        bsdmainutils cron vesta vesta-nginx vesta-php expect libmail-dkim-perl
+        unrar-free vim-common vesta-ioncube vesta-softaculous net-tools unzip"
+fi
 
 # Defining help function
 help() {
