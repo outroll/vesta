514bf9e0f10a884452e0cc4b8d5e51340aa13b3b
myvesta <38690722+myvesta@users.noreply.github.com>
2020-06-11 16:09:42 +0200

    Create fix_ssl_directive_in_templates.sh

---
+++ upd/fix_ssl_directive_in_templates.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+release=$(cat /etc/debian_version | tr "." "\n" | head -n1)
+
+if [ "$release" -eq 9 ] || [ "$release" -eq 10 ]; then
+    for FILE in /usr/local/vesta/data/templates/web/nginx/*.stpl; do
+        check_grep=$(grep -c 'http2' $FILE)
+        if [ "$check_grep" -eq 0 ]; then
+            echo "=== Fixing ssl directive in $FILE"
+            sed -i "s|:%proxy_ssl_port%;|:%proxy_ssl_port% ssl http2;|g" $FILE
+            sed -i "s|ssl *on;|#ssl on;|g" $FILE
+        fi
+    done
+fi
+
+for user in $(grep '@' /etc/passwd |cut -f1 -d:); do
+    if [ ! -f "/usr/local/vesta/data/users/$user/user.conf" ]; then
+        continue;
+    fi
+    v-rebuild-web-domains $user 'no'
+done
