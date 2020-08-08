348cecb0e98e34307877aa508071ba7ae5b7531b
myvesta <38690722+myvesta@users.noreply.github.com>
2020-08-08 23:34:04 +0200

    Update convert-vesta-to-myvesta.sh

--- src/deb/for-download/tools/convert-vesta-to-myvesta.sh
+++ src/deb/for-download/tools/convert-vesta-to-myvesta.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # *** Debian only ***
+# This script is NOT recommended, because a lot of myVesta features will not be added to server this way.
+# Better way is described here - https://forum.myvestacp.com/viewtopic.php?f=14&t=50
 
 wget -O - http://apt.myvestacp.com/deb_signing.key | sudo apt-key add -
 codename="$(cat /etc/os-release |grep VERSION= |cut -f 2 -d \(|cut -f 1 -d \))"
