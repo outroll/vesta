ea0fa49d0e9ec738ff6f82ea03790d4502ac40be
myvesta <38690722+myvesta@users.noreply.github.com>
2020-08-30 05:06:45 +0200

    Update string.php

--- func/string.php
+++ func/string.php
@@ -1,11 +1,11 @@
 <?php
 
 function myvesta_replace_in_file($file, $find, $replace) {
-    if (!file_exists($file)) return myvesta_throw_error (3, "File '$file' not found");
+    if (!file_exists($file)) return myvesta_throw_error (MYVESTA_ERROR_FILE_DOES_NOT_EXISTS, "File '$file' not found");
 
     $buf=file_get_contents($file);
 
-    if (strpos($buf, $find)===false) return myvesta_throw_error (4, "String '$find' not found");
+    if (strpos($buf, $find)===false) return myvesta_throw_error (MYVESTA_ERROR_STRING_DOES_NOT_EXISTS, "String '$find' not found");
 
     $buf=str_replace($find, $replace, $buf);
     $r=file_put_contents($file, $buf);
