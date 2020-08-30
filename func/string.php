3919840fc42c7eecbb605d64ff6a0b903782e6e7
myvesta <38690722+myvesta@users.noreply.github.com>
2020-08-30 05:09:18 +0200

    Update string.php

--- func/string.php
+++ func/string.php
@@ -1,6 +1,6 @@
 <?php
 
-function myvesta_replace_in_file($file, $find, $replace) {
+function myvesta_replace_in_file($find, $replace, $file) {
     if (!file_exists($file)) return myvesta_throw_error (MYVESTA_ERROR_FILE_DOES_NOT_EXISTS, "File '$file' not found");
 
     $buf=file_get_contents($file);
