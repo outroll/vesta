f31b4b4d41b1921fd82d1bb355635e8dd005f7f2
myvesta <38690722+myvesta@users.noreply.github.com>
2020-12-12 13:52:26 +0100

    Update index.php

--- web/logout/index.php
+++ web/logout/index.php
@@ -4,10 +4,11 @@ session_start();
 
 if (!empty($_SESSION['look'])) {
     unset($_SESSION['look']);
+    header("Location: /");
 } else {
     session_destroy();
+    header("Location: /login/");
 }
 
-header("Location: /login/");
 exit;
 ?>
